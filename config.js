2b077ad5edbd454251aff4b744e1bfc533c55c75
xiaowei hu <billhu422@126.com>
2017-09-08 15:45:36 +0800

    Cut down routes.service code

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var config = {};
 config.biz = {};
 config.qcloud = {};
@@ -49,39 +48,6 @@ config.aliyun.apiParamsMapping = {
     signatureMethod: 'SignatureMethod',
     timestamp: 'Timestamp',
     nonce:'SignatureNonce'
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-config.oauth = {
-    account_server: 'http://124.251.62.217:8000',
-    //account_server: 'http://192.168.87.152:8000',
-    client_id: '9c9bca6d26234ff98fb93df3e3d065c5',
-    client_secret: 'e1396a545ace426a941c0edfd3403d0f',
-    grant_type : 'password',
-    username : 'laoguo.cn@gmail.com',
-    email:'laoguo.cn@gmail.com',
-    password: '12345',
-};
-
-
-
-config.eco = {
-    baseUrl: 'http://124.251.62.215:8000',
-    orderPath: '/DSProductOrdering/api/productOrdering/v2/productOrder'
-};
-
-config.dbRest = {
-    baseUrl : 'http://localhost:3003',
-};
-
-config.qcloud = {
-    SecretId: 'AKID2sYy826AMHzTjoMHemobCcXHm47vLoul',
-    SecretKey: 'mLjxwgDVebNtEKEvPeePuBCxTjlopfGg',
-};
-
-config.aliyun = {
-    SecretId: 'LTAIzgYvg0hVfNMK',
-    SecretKey: '3ExrKEf611ODomcjdpWus8mX4JIsJ4',
 };
 
 module.exports = config;
